377153ca80ec6bd6fd8b2a794bb62154be1fb42e
yiconghuang-ucb <yicong_huang@berkeley.edu>
2026-08-19 10:59:43 -0700

    Add basic implementation of Newton's method.

---
+++ Test.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import newton
+import numpy as np
+
+x_0 = 2.5
+
+result = newton.optimize(x_0,np.cos)
+
+print(result)
+
+# %%
---
+++ newton.py
@@ -0,0 +1,36 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+def derivative(f, x):
+    h = 1e-5
+    return (f(x + h) - f(x)) / h
+
+def second_derivative(f, x):
+    return derivative(lambda z: derivative(f, z), x)
+
+def optimize(x0, f):
+    tolerance = 1e-6
+    max_iterations = 100
+    x = x0
+    for i in range(max_iterations):
+        d1 = derivative(f, x)
+        d2 = second_derivative(f, x)
+        x_new = x - d1 / d2
+        if abs(x_new - x) < tolerance:
+            break
+        x = x_new
+    return x
+
+# %%
